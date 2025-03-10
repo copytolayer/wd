e8c5335d719b3791b25d94bcd04ec62e2a0a48b4
copytolayer <7138ye@naver.com>
2025-03-10 19:29:35 +0900

    task9 printing 추가

--- test.py
+++ test.py
@@ -7,8 +7,13 @@ print("case1, bye")
 print("case2, hi")
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("function1")
 print("task2")
 =======
 print("task8")
 >>>>>>> 7f1f5b7 (task8 printing 추가)
+=======
+print("task8")
+print("task9")
+>>>>>>> d551394 (task9 printing 추가)
